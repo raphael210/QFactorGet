6ccc626226a5c44b410bb7627c88659dda641319
raphael210 <raphael210@163.com>
2016-09-09 11:30:32 +0800

    clean up the namespace

--- R/QFactorGet-package.r
+++ R/QFactorGet-package.r
@@ -2,7 +2,5 @@
 #'
 #' @name QFactorGet
 #' @docType package
-#' @import QDataGet QUtility dplyr RODBC DBI RSQLite 
-#' @importFrom reshape2 acast dcast melt
-#' @importFrom plyr ddply ldply laply
+#' @import QDataGet QUtility  
 NULL
