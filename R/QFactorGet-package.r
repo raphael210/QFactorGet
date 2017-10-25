e0ec7478386782ecb65bc1724690d7a2e024f993
QuantAndrew <taoaming1001@163.com>
2017-10-25 14:41:23 +0800

    Delete QFactorGet-package.r

--- R/QFactorGet-package.r
+++
@@ -1,9 +0,0 @@
-#' QFactorGet
-#'
-#' @name QFactorGet
-#' @docType package
-#' @import QDataGet QUtility  
-#' @importFrom DBI dbDriver dbGetQuery dbSendQuery dbClearResult dbWriteTable dbRemoveTable dbConnect dbDisconnect dbListFields dbListTables
-#' @importFrom RODBC odbcConnect odbcDriverConnect odbcClose odbcCloseAll sqlQuery sqlDrop sqlSave
-#' @importFrom dplyr %>% group_by
-NULL
